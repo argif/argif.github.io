c080ba3f173362d4895a5d1fed60ac6644d5ce05
a-ill <dumpy.dumpster@yandex.com>
2022-03-07 21:08:58 +0200

    Update

--- tasks.js
+++ tasks.js
@@ -1,95 +1,157 @@
-window.onbeforeunload = function(){
-    console.log("here")
-    localStorage.clear()
- }
 
-
-console.log(localStorage)
-
-localStorage.setItem("name","John")
-
-/*
-if (localStorage.length==0) {
-
-    var links_total = 3
-    var ads_total = 1
-    var seconds_total = 1000
-    var links_done = 0
-    var ads_done = 0
-    var seconds_done = 0
-
-    localStorage.setItem("links_total",links_total)
-    localStorage.setItem("ads_total",ads_total)
-    localStorage.setItem("seconds_total",seconds_total)
-
-    localStorage.setItem("links_done",links_done)
-    localStorage.setItem("ads_done",ads_done)
-    localStorage.setItem("seconds_done",seconds_done)
-}
-else {
-    var links_total = parseFloat(localStorage.getItem("links_total"))
-    var ads_total = parseFloat(localStorage.getItem("ads_total"))
-    var seconds_total = parseFloat(localStorage.getItem("seconds_total"))
-
-    var links_done = parseFloat(localStorage.getItem("links_done"))
-    var ads_done = parseFloat(localStorage.getItem("ads_done"))
-    var seconds_done = parseFloat(localStorage.getItem("seconds_done"))
-}
-*/
-/*
-function updateLinks() {
-    links_done += 1
-    var counter = document.getElementById("links-to-click")
-    var dif = links_total - links_done
-    if (dif>=0) {
-        counter.innerHTML = dif
-        localStorage.setItem("links_done",links_done)
+function checkVisibility() {
+    if (sessionStorage.visibility==null) {
+        sessionStorage.visibility = "hidden"
+        document.getElementById("task-banner").style.visibility = "hidden"
+    }
+    else {
+        document.getElementById("task-banner").style.visibility = sessionStorage.visibility
     }
 }
 
-function updateAds() {
-    ads_done += 1
-    var counter = document.getElementById("ads-to-click")
-    var dif = ads_total - ads_done
-    if (dif>=0) {
-        counter.innerHTML = dif
-        localStorage.setItem("ads_done",ads_done)
+
+function startup() {
+    if (sessionStorage.length<6) {
+        var links_total = Math.floor(Math.random()*4+1)
+        var ads_total = Math.floor(Math.random()*3+1)
+        var seconds_total = Math.floor(Math.random()*80 + 10)
+        var links_done = 0
+        var ads_done = 0
+        var seconds_done = 0
+
+        sessionStorage.setItem("visibility","hidden")
+        sessionStorage.setItem("links_total",links_total)
+        sessionStorage.setItem("ads_total",ads_total)
+        sessionStorage.setItem("seconds_total",seconds_total)
+
+        sessionStorage.setItem("links_done",links_done)
+        sessionStorage.setItem("ads_done",ads_done)
+        sessionStorage.setItem("seconds_done",seconds_done)
     }
+}
+
+function updateLinks(event) {
+    var dif = parseFloat(sessionStorage.links_total - sessionStorage.links_done)
+    if (dif>0) {
+        sessionStorage.links_done = parseFloat(sessionStorage.links_done) + 1
+        var counter = document.getElementById("links-to-click")
+        counter.innerHTML = dif - 1
+    }
+    event.stopPropagation()
+}
+
+function updateAds(event) {
+    var dif = parseFloat(sessionStorage.ads_total - sessionStorage.ads_done)
+    if (dif>0) {
+        sessionStorage.ads_done = parseFloat(sessionStorage.ads_done) + 1
+        var counter = document.getElementById("ads-to-click")
+        counter.innerHTML = dif - 1
+    }
+    event.srcElement.onmousedown = null
 }
 
 function updateSeconds() {
-    seconds_done += 1
-    var counter = document.getElementById("seconds-to-stay")
-    var dif = seconds_total - seconds_done
-    //console.log(dif)
-    if (dif>=0) {
-        counter.innerHTML = dif
-        localStorage.setItem("seconds_done",seconds_done)
+    var dif = parseFloat(sessionStorage.seconds_total - sessionStorage.seconds_done)
+    if (dif>0) {
+        sessionStorage.seconds_done = parseFloat(sessionStorage.seconds_done) + 1
+        var counter = document.getElementById("seconds-to-stay")
+        counter.innerHTML = dif - 1
     }
 }
 
 function startCounter() {
-    setInterval(updateTasks, 1000)
+    setInterval(updateSeconds, 1000)
+    setInterval(checkFinished, 1000)
 }
 
 function initTasks() {
+    startup()
     var counter = document.getElementById("links-to-click")
-    counter.innerHTML = links_total
+    counter.innerHTML = sessionStorage.links_total - sessionStorage.links_done
     counter = document.getElementById("ads-to-click")
-    counter.innerHTML = ads_total
+    counter.innerHTML = sessionStorage.ads_total - sessionStorage.ads_done
     counter = document.getElementById("seconds-to-stay")
-    counter.innerHTML = seconds_total
+    counter.innerHTML = sessionStorage.seconds_total - sessionStorage.seconds_done
 
-    var all_links = document.querySelectorAll("a")
-    for (var i=0; i<all_links.length;i++) {
-        all_links[0].onclick = function(event) { 
-            updateLinks() 
-        }
-    }
+    attachLinksSnooper()
+    attachAdsSnooper()
     startCounter()
 }
 
-function updateTasks() {
-    updateSeconds()
+function attachLinksSnooper() {
+    var all_links = document.querySelectorAll("a")
+    var linkStates = sessionStorage.linkStates
+    if (linkStates==null) {
+        sessionStorage.linkStates = JSON.stringify(Array(all_links.length).fill(true))
+    }
+    for (var i=0; i<all_links.length;i++) {
+        if (JSON.parse(sessionStorage.linkStates)[i]) {
+            var link = all_links[i]
+            link.onmousedown = function(event) {
+                updateLinks(event)
+            }
+            link.addEventListener('mousedown', function (event) {
+                var src = event.srcElement
+                if (src.onmousedown!=null) {
+                    src.onmousedown=null
+                }
+                var all_links = document.querySelectorAll("a")
+                for (var j=0; j<all_links.length;j++) {
+                    if (all_links[j]==src) {
+                        var newLinkStates = JSON.parse(sessionStorage.linkStates)
+                        newLinkStates[j] = false
+                        sessionStorage.linkStates = JSON.stringify(newLinkStates)
+                        break
+                    }
+                }
+                //var url = event.target.href;
+                //window.open(url, '_self');
+                //event.preventDefault();
+            })
+        }
+    }
 }
-*/
+
+function attachAdsSnooper() {
+    var all_ads = document.querySelectorAll("div.ads")
+    var adStates = sessionStorage.adStates
+    if (adStates==null) {
+        sessionStorage.adStates = JSON.stringify(Array(all_ads.length).fill(true))
+    }
+    for (var i=0; i<all_ads.length;i++) {
+        if (JSON.parse(sessionStorage.adStates)[i]) {
+            var ad = all_ads[i]
+            ad.onmousedown = function(event) {
+                updateAds(event)
+            }
+            ad.addEventListener('mousedown', function (event) {
+                var src = event.srcElement
+                if (src.onmousedown!=null) {
+                    src.onmousedown=null
+                }
+                var all_ads = document.querySelectorAll("a")
+                for (var j=0; j<all_ads.length;j++) {
+                    if (all_ads[j]==src) {
+                        var newAdStates = JSON.parse(sessionStorage.adStates)
+                        newAdStates[j] = false
+                        sessionStorage.adStates = JSON.stringify(newAdStates)
+                        break
+                    }
+                }
+                //event.preventDefault();
+            })
+        }
+    }
+}
+
+
+function checkFinished() {
+    var links = sessionStorage.links_total - sessionStorage.links_done
+    var ads = sessionStorage.ads_total - sessionStorage.ads_done
+    var seconds = sessionStorage.seconds_total - sessionStorage.seconds_done
+    if (links==0 & ads==0 & seconds==0) {
+        document.getElementById("task-banner").innerHTML = '(1): 1<br class="task">(2): 2<br class="task">(3): 1<br class="task">(4): 3<br class="task">(5): 2'
+    }
+
+}
+
